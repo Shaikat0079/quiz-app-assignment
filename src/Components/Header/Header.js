85ca618994d83e3711f4a5f5f15e7daa17919dcf
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-11 00:12:59 +0600

    Nav Bar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h2 className='text-2xl'>This is head!</h2>
+            <nav className='flex justify-between items-center'>
+                <div>
+                    <h2 className=' text-4xl text-rose-500'>JS Quiz App</h2>
+                </div>
+                <div className='flex gap-6'>
+                    <nav className='text-2xl text-rose-600'>Topics</nav>
+                    <nav className='text-2xl text-rose-600'>Statics</nav>
+                    <nav className='text-2xl text-rose-600'>Blog</nav>
+                </div>
+            </nav>
         </div>
     );
 };
