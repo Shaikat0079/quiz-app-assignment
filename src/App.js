7101f749baeb225eb05aa679c61a15af7974a1f4
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-10 23:56:46 +0600

    Headed and Layout Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+    }
+  ])
   return (
-    <div className=''>
+    <RouterProvider router={router}>
 
-    </div>
+    </RouterProvider>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>This is head!</h2>
+        </div>
+    );
+};
+
+export default Header;
