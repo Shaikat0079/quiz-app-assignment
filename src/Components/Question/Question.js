957c5cf1d0d66b256f1f60e16330ff13a40bd691
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-11 04:37:16 +0600

    Questions Loaded

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import QuestionAndOption from '../QuestionAndOption/QuestionAndOption';
 
 const Question = () => {
+    const quiz = useLoaderData();
+    const { questions, logo, name, id } = quiz.data;
+    // console.log(questions[0].options);
+    // {
+    //     questions.map(question => console.log(question))
+    // }
     return (
         <div>
-            <h2>This is loading!</h2>
+            {/* {questions.length} */}
+            <h1 className='text-6xl text-center'>Welcome to {name} Section!</h1>
+            <img className='m-12 mx-auto' src={logo} alt="" />
+            {
+                questions.map(question => <div>{question.question}</div>)
+            }
+            {/* <p>{questions.map(q => q)}</p> */}
         </div>
     );
 };
---
+++ src/Components/QuestionAndOption/QuestionAndOption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuestionAndOption = ({ questions }) => {
+    // const { queston } = questions
+    return (
+        <div>
+            {/* <p>{queston.question}</p> */}
+        </div>
+    );
+};
+
+export default QuestionAndOption;
