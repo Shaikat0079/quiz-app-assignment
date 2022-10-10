5d1d3fd539523252843372d4c59a847bcf85a162
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-11 02:39:58 +0600

    Design of Topics Added

--- src/Components/Topic/Topic.js
+++ src/Components/Topic/Topic.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Topic = ({ topic }) => {
-    const { name, logo } = topic
+    const { name, logo, total } = topic
     return (
-        <div className='border-2 bg-red-500'>
+        <div className='bg-red-500 w-96 p-4'>
             <img src={logo} alt="" srcset="" />
-            <p>{name}</p>
+            <div className='flex justify-between items-center m-4 px-6'>
+                <p className='text-white'>{name}</p>
+                <p className='text-white font-bold'>Q-No:{total}</p>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    Button
+                </button>
+            </div>
         </div>
     );
 };
