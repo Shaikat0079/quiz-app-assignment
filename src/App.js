091fb8869f5fa69cb224c9723f22b95826b5ff49
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-11 00:33:34 +0600

    Links added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blogs from './Components/Blogs/Blogs';
+import Statistics from './Components/Statistics/Statistics';
+import Topics from './Components/Topics/Topics';
 import Main from './Layout/Main';
 
 function App() {
@@ -7,7 +10,26 @@ function App() {
     {
       path: '/',
       element: <Main></Main>,
-    }
+      children: [
+        {
+          path: '/',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: 'statistics',
+          element: <Statistics></Statistics>
+        }
+        ,
+        {
+          path: 'blog',
+          element: <Blogs></Blogs>
+        }
+      ]
+    },
   ])
   return (
     <RouterProvider router={router}>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -8,9 +9,9 @@ const Header = () => {
                     <h2 className=' text-4xl text-rose-500'>JS Quiz App</h2>
                 </div>
                 <div className='flex gap-6'>
-                    <nav className='text-2xl text-rose-600'>Topics</nav>
-                    <nav className='text-2xl text-rose-600'>Statics</nav>
-                    <nav className='text-2xl text-rose-600'>Blog</nav>
+                    <nav className='text-2xl text-rose-600'><Link to='/topics'>Topics</Link></nav>
+                    <nav className='text-2xl text-rose-600'><Link to='/statistics'>Statics</Link></nav>
+                    <nav className='text-2xl text-rose-600'><Link to='/blog'>Blog</Link></nav>
                 </div>
             </nav>
         </div>
