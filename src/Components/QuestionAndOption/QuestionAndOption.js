cdd24658ce7836c22d4fdf475cfeccc4d746246b
Shaikat0079 <baruashaikat00790@gmail.com>
2022-10-11 16:14:54 +0600

    Question Checked and Tost added according to the right or wrong answer

--- src/Components/QuestionAndOption/QuestionAndOption.js
+++ src/Components/QuestionAndOption/QuestionAndOption.js
@@ -4,10 +4,8 @@ import { EyeIcon } from '@heroicons/react/24/solid'
 
 const QuestionAndOption = ({ qus }) => {
     const { options, id, correctAnswer, question } = qus;
+    const clicked = (event) => { event.currentTarget.textContent === correctAnswer ? toast('You Got The Correct One') : toast('Try Again') }
     const notify = () => toast(correctAnswer);
-    // const [answer,showAnswer]=useState([]);
-    // const { queston } = question
-    // console.log(question.option)
 
     return (
         <div className='m-6 bg-green-400 p-6 flex justify-center'>
@@ -17,11 +15,18 @@ const QuestionAndOption = ({ qus }) => {
                     <EyeIcon onClick={notify} className="h-6 w-6 text-black-500" />
                     <ToastContainer />
                 </div>
-                <div className=''>
+                <ul className=''>
                     {
-                        options.map(option => <p className='m-2' key={option} >{option}</p>)
+                        options.map(option => <div key={option} className="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                            <li onClick={clicked} className="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">
+                                <div className="flex items-center pl-3">
+                                    <input id="list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                    <label htmlFor="list-radio-license" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{option}</label>
+                                </div>
+                            </li>
+                        </div>)
                     }
-                </div>
+                </ul>
             </div>
         </div>
     );
